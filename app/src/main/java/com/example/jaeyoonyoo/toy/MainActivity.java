021c94064ec7ceacbae5bfd0d6bf81ae6bd0e69c
jaeyoonyoo <yooasd11@hotmail.com>
2018-09-12 22:26:12 +0900

    Initial commit

---
+++ app/src/main/java/com/example/jaeyoonyoo/toy/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.jaeyoonyoo.toy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        EditText editText = findViewById(R.id.edit);
+        editText.clearFocus();
+    }
+}
